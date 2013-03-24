c8160f9e44156692c35000afac476ddda030b4b5
Harsimran Singh Maan <hsmaan@hsmaanNodeJs.(none)>
2013-03-24 02:18:09 -0700

    Fix home page

--- routes/home.js
+++ routes/home.js
@@ -3,13 +3,29 @@
  * GET users listing.
  */
 var persons = [
-                { "name": "Rachael Smith", "img": "Rachael.jpeg", "rating": 10, "time": "32 mins" }
-, { "name": "Miranda Walters", "img": "miranda.jpeg", "rating": 6, "time": "15 mins" }
-, { "name": "Nick Williams", "img": "nick.jpeg", "rating": 9, "time": "18 mins" }
-, { "name": "Nancy Drew", "img": "Nancy.jpeg", "rating": 7, "time": "15 mins" }
-, { "name": "Saurabh Dutt", "img": "saurabh.jpeg", "rating": 7, "time": "30 mins" }
-, { "name": "Thomas Tanks", "img": "thomas.jpeg", "rating": 2, "time": "26 mins" }
+                { name: "Rachael Smith", img: "Rachael.jpg", rating: 10, time: "32 mins" }
+, { name: "Miranda Walters", img: "Miranda.jpg", rating: 6, time: "15 mins" }
+, { name: "Nick Williams", img: "Nick.jpg", rating: 9, time: "18 mins" }
+, { name: "Nancy Drew", img: "Nancy.jpg", rating: 7, time: "15 mins" }
+, { name: "Saurabh Dutt", img: "Saurabh.jpg", rating: 7, time: "30 mins" }
+, { name: "Thomas Tanks", img: "Thomas.jpg", rating: 2, time: "26 mins" }
 ];
+
+function compare(a,b) {
+  if (a.time < b.time)
+     return 1;
+  if (a.time < b.time)
+    return -1;
+  if (a.rating > b.rating)
+     return -1;
+  if (a.rating < b.rating)
+    return 1;
+  return 0
+}
 exports.index = function(req, res){
-    res.render('home', { title: 'Line Hero - ' + req.username, persons: persons.every(function (person) { return person.name!=req.username }) });
+	var name=req.params.username;
+console.log(name);
+	var tempPersons=persons.filter(function (person) { return person.name.replace(" ","").toLowerCase()!=name.toLowerCase(); }).sort(compare);
+	
+    res.render('home', { title: 'Line Hero - '+name, persons:tempPersons , naam:name });
 };
