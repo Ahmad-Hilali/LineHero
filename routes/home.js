1ecd3a8ea6d8a549f5d7fedb178fc99c2e863dd0
Harsimran Singh Maan <maan.harry@gmail.com>
2013-03-24 01:08:39 -0700

    Add home page

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,14 @@
 /*
  * GET users listing.
  */
-
+var persons = [
+                { "name": "Rachael Smith", "img": "Rachael.jpeg", "rating": 10, "time": "32 mins" }
+, { "name": "Miranda Walters", "img": "miranda.jpeg", "rating": 6, "time": "15 mins" }
+, { "name": "Nick Williams", "img": "nick.jpeg", "rating": 9, "time": "18 mins" }
+, { "name": "Nancy Drew", "img": "Nancy.jpeg", "rating": 7, "time": "15 mins" }
+, { "name": "Saurabh Dutt", "img": "saurabh.jpeg", "rating": 7, "time": "30 mins" }
+, { "name": "Thomas Tanks", "img": "thomas.jpeg", "rating": 2, "time": "26 mins" }
+];
 exports.index = function(req, res){
-  res.send("respond with a resource");
+    res.render('home', { title: 'Line Hero - ' + req.username, persons: persons.every(function (person) { return person.name!=req.username }) });
 };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Line Hero' });
+    res.render('index', { title: 'Line Hero' });
 };
