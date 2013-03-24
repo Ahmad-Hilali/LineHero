285972ed127eadb05ff4a06089bd6662bdf421a1
Harsimran Singh Maan <hsmaan@hsmaanNodeJs.(none)>
2013-03-24 13:50:02 -0700

    Add modal

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,19 @@
  * GET users listing.
  */
 var persons = [
-                { name: "Rachael Smith", img: "Rachael.jpg", rating: 10, time: "32 mins", location:"@SuperStore" }
-, { name: "Miranda Walters", img: "Miranda.jpg", rating: 6, time: "15 mins", location: "@SuperStore" }
-, { name: "Nick Williams", img: "Nick.jpg", rating: 9, time: "18 mins", location: "@SuperStore" }
-, { name: "Nancy Drew", img: "Nancy.jpg", rating: 7, time: "15 mins", location: "@SuperStore" }
-, { name: "Saurabh Dutt", img: "Saurabh.jpg", rating: 7, time: "30 mins", location: "@SuperStore" }
-, { name: "Thomas Tanks", img: "Thomas.jpg", rating: 2, time: "26 mins", location: "@SuperStore" }
+                { name: "Rachael Smith", img: "Rachael.jpg", rating: 10, time: "32 mins", location:"@SuperStoreMT",num:2 }
+, { name: "Miranda Walters", img: "Miranda.jpg", rating: 6, time: "15 mins", location: "@SuperStoreMT",num:14 }
+, { name: "Nick Williams", img: "Nick.jpg", rating: 9, time: "18 mins", location: "@SuperStoreMT" ,num:11}
+, { name: "Nancy Drew", img: "Nancy.jpg", rating: 7, time: "15 mins", location: "@StarbuckMTFc",num:13 }
+, { name: "Saurabh Dutt", img: "Saurabh.jpg", rating: 7, time: "30 mins", location: "@StarbuckMTFc",num:1 }
+, { name: "Thomas Tanks", img: "Thomas.jpg", rating: 2, time: "26 mins", location: "@SuperStoreMT",num:5 }
 ];
 
 function compare(a,b) {
+ if (a.location < b.location)
+     return 1;
+  if (a.location > b.location)
+	return -1;
   if (a.time < b.time)
      return 1;
   if (a.time < b.time)
@@ -22,6 +26,7 @@ function compare(a,b) {
     return 1;
   return 0
 }
+
 exports.index = function(req, res){
 	var name=req.params.username;
 console.log(name);
